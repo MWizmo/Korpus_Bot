04ae61af651fde9db8b138b1bfe16dd4447cdca6
Andrew Kuzin <AndrewKuzin@bitbucket.org>
2020-05-15 17:17:31 +0500

    New weekly voting system

--- app/__init__.py
+++ app/__init__.py
@@ -6,8 +6,9 @@ import bot_config
 app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://korpus_user:korpus_password@localhost/korpus_db'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:1@localhost/korpus_db'
 db = SQLAlchemy(app)
 bot = telebot.TeleBot(bot_config.token)
-bot.set_webhook('https://bot.eos.korpus.io/tg')
+#bot.set_webhook('https://bot.eos.korpus.io/tg')
 from app.routes import blueprint
 app.register_blueprint(blueprint)
