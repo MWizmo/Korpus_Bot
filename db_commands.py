8a1211f84dabead6e9da8ec8b6b6e45b35d6ea5c
Andrew Kuzin <AndrewKuzin@bitbucket.org>
2020-06-12 15:31:25 +0500

    Weekly voting alert

--- db_commands.py
+++ db_commands.py
@@ -43,6 +43,7 @@ def getAdminKeyboard():
     keyboard = ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
     keyboard.add(alert_form_btn)
     keyboard.add(alert_voting_btn)
+    keyboard.add(weekly_vote_btn)
     keyboard.add(back_btn)
     return keyboard
 
