b2309d3158271b9c31cc1e1c26ffa872b06a4975
MWizmo <masterwizmoo@yandex.ru>
2022-02-15 21:07:02 +0500

    new token

--- bot_config.py
+++ bot_config.py
@@ -1,2 +1,2 @@
-token = '573817226:AAHM6cFFyr64GS7c5ZWw6z_j6UHGNKQldBU' #KorpusBot (with avatar)
+token = '573817226:AAGQM1FP1qznVJ137fKX_wy94ntb6q5bQVg' #KorpusBot (with avatar)
 # token = '632250139:AAEa_80FLxkKCJEmzzKnuEu3oek8wV3Y8Ek'  # Test bot
