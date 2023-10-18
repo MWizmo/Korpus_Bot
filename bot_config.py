a0ccff54c32df5b4c6fcc68c8b26ebcabbf29971
Andrey Kuzin <masterwizmoo@yandex.ru>
2023-10-18 14:28:14 +0500

    jira values

--- bot_config.py
+++ bot_config.py
@@ -1,4 +1,4 @@
 token = '573817226:AAGQM1FP1qznVJ137fKX_wy94ntb6q5bQVg' #KorpusBot (with avatar)
 # token = '632250139:AAEa_80FLxkKCJEmzzKnuEu3oek8wV3Y8Ek'  # Test bot
-jira_username = 'jira_username'
-jira_token = 'jira_token'
+jira_username = 'masterwizmoo@yandex.ru'
+jira_token = 'ATATT3xFfGF0CrI3XoDF4lx1i7vdCeGU_b6KShZUbCVViTi-Dv0Y5wvu9WaGpwJNX8Kh0JdeH4vLqf665MrtKmkk5Y7w6LHeRB5g4n7usWi4Fym0zN7pygroL5jyXzv7KXXujOodoKPSFWpRSmEARk8zfFXe6tzJMGv_GICESpUfXtggpub6nSQ=24B88347'
